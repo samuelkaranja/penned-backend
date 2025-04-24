e8629a257a6cf18e4df8a7c86eafab140e0bc812
samuelkaranja <samuelkaranja007@gmail.com>
2025-04-24 23:50:13 +0300

    Fixed the issue in the LoginView

--- api/views.py
+++ api/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+
 
 
 
