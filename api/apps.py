646402993cf6f9cb8cde6d7c2d9f008d683a853f
samuelkaranja <samuelkaranja007@gmail.com>
2025-05-05 23:27:44 +0300

    Fixed the issue in the signals.py file

--- api/apps.py
+++ api/apps.py
@@ -1,9 +1,8 @@
 from django.apps import AppConfig
 
-
 class ApiConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'api'
 
-def ready(self):
-    import api.signals
+    def ready(self):  # Make sure this is inside the class!
+        import api.signals
